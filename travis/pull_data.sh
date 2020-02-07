0a2ab57eea633e3b15b95af0eedfa1e5f0be2f9f
jbradl11 <jhbradley@mitre.org>
2020-02-07 11:51:00 -0500

    Update bash file

--- travis/pull_data.sh
+++ travis/pull_data.sh
@@ -3,8 +3,20 @@
 export WORKING_DIR='pwd'
 echo "> Working dir: $WORKING_DIR"
 
-echo "> Getting data..."
+echo "> Cloning FHIR Bundle Calculator..."
 git clone https://github.com/projecttacoma/fhir-bundle-calculator.git
+
+echo "> Cloning FHIR Generated Patients..."
+git clone https://github.com/projecttacoma/fhir-patient-generator.git
+
+cd fhir-patient-generator
+
+echo ">Installing FHIR Patient Generator CLI..."
+npm install -g fhir-bundle-calculator
+
+echo ">Calculate Bundles..."
+calculate-bundles -d ./EXM_130/patients-r4/
+
 
 echo "> Making data dir"
 sudo mv fhir-bundle-calculator data
