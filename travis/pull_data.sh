e159643017fde9df0c8ba2e7390ecf8bd8c56343
jbradl11 <jhbradley@mitre.org>
2020-02-10 18:40:43 -0500

    Update pull_data.sh

--- travis/pull_data.sh
+++ travis/pull_data.sh
@@ -9,20 +9,13 @@ git clone https://github.com/projecttacoma/fhir-bundle-calculator.git
 echo "> Cloning FHIR Generated Patients..."
 git clone https://github.com/projecttacoma/fhir-patient-generator.git
 
-cd fhir-patient-generator
-
+#cd fhir-patient-generator
 echo ">Installing FHIR Patient Generator CLI..."
 npm install -g fhir-bundle-calculator
 
-cd EXM_130
-
 echo ">Calculate Bundles..."
-
-make connectathon
-make .seed-cqf-ruler-r4
-
-cd ..
-cd ..
+make ./fhir-patient-generator/EXM_130/connectathon
+make ./fhir-patient-generator/EXM_130.seed-cqf-ruler-r4
 
 calculate-bundles -d ./fhir-patient-generator/EXM_130/patients-r4/denominator/ -c ./pages/cql/in-progress/fhir4/EXM130_FHIR4-7.2.000.cql -u http://localhost:8080/cqf-ruler-r4/fhir
 calculate-bundles -d ./fhir-patient-generator/EXM_130/patients-r4/numerator/ -c ./pages/cql/in-progress/fhir4/EXM130_FHIR4-7.2.000.cql -u http://localhost:8080/cqf-ruler-r4/fhir
