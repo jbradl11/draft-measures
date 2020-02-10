e9458cb6a67eb177015ebd6bd027597cfdd44336
jbradl11 <jhbradley@mitre.org>
2020-02-10 13:30:05 -0500

    Update pull_data.sh

--- travis/pull_data.sh
+++ travis/pull_data.sh
@@ -21,6 +21,8 @@ echo ">Calculate Bundles..."
 make .seed-cqf-ruler-r4
 
 cd ..
+cd ..
+
 calculate-bundles -d ./fhir-patient-generator/EXM_130/patients-r4/denominator/ -c ./pages/cql/in-progress/fhir4/EXM130_FHIR4-7.2.000.cql -u http://localhost:8080/cqf-ruler-r4/fhir
 calculate-bundles -d ./fhir-patient-generator/EXM_130/patients-r4/numerator/ -c ./pages/cql/in-progress/fhir4/EXM130_FHIR4-7.2.000.cql -u http://localhost:8080/cqf-ruler-r4/fhir
 
